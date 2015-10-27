f966ffbbb366ac328e21fdd727e21c19e4ded10f
Dirk Baeumer <dirkb@microsoft.com>
2015-10-27 15:30:30 +0100

    Better server folder name

--- eslint/extension.ts
+++ eslint/extension.ts
@@ -1,12 +1,12 @@
 import * as path from 'path';
-import { window, workspace, commands } from 'vscode'; 
+import { window, workspace, commands } from 'vscode';
 import { LanguageClient, ClientOptions, ClientStarter, RequestType } from 'vscode-languageclient';
 
-export function activate() { 
+export function activate() {
 
 	// We need to go one level up since an extension compile the js code into
 	// the output folder.
-	let module = path.join(__dirname, '..', 'bin', 'server', 'server.js');
+	let module = path.join(__dirname, '..', 'server', 'server.js');
 	let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
 	let clientOptions: ClientOptions = {
 		server: {
