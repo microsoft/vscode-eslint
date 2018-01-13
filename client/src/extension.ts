a750bf0f1a89c26befbbaa45408b7e5582d724e9
Josh Unger <joshunger1@gmail.com>
2018-01-13 06:23:28 -0800

    Modify second error message when eslint can't be found
    Modify error message when eslint can't be found to improve the user experience.  Since npm is the default setting but most people are using yarn (see state of js data) including the vs code examples, add "If you are using yarn instead of npm set the setting `"eslint.packageManager": "yarn"`".  An even more improved user experience would be to check both.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -650,6 +650,8 @@ export function realActivate(context: ExtensionContext) {
 						`To use ESLint please install eslint by running \'npm install eslint\' in the workspace folder ${workspaceFolder.name}`,
 						'or globally using \'npm install -g eslint\'. You need to reopen the workspace after installing eslint.',
 						'',
+						'If you are using yarn instead of npm set the setting `"eslint.packageManager": "yarn"`',
+						'',
 						`Alternatively you can disable ESLint for the workspace folder ${workspaceFolder.name} by executing the 'Disable ESLint' command.`
 					].join('\n'));
 				}
