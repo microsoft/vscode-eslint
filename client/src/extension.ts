79a9b34ec65d1be3bf16c113e88ef4c76dd9fce1
hantatsang <hantatsang@gmail.com>
2021-04-06 22:31:47 +1000

    feat: update eslint library message

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1853,7 +1853,7 @@ function realActivate(context: ExtensionContext): void {
 					`To use ESLint please install eslint by running ${localInstall[packageManager]} in the workspace folder ${workspaceFolder.name}`,
 					`or globally using '${globalInstall[packageManager]}'. You need to reopen the workspace after installing eslint.`,
 					'',
-					isPackageManagerNpm ? 'If you are using yarn or pnpm instead of npm set the setting `eslint.packageManager` or `npm.packageManager` to either `yarn` or `pnpm`' : null,
+					isPackageManagerNpm ? 'If you are using yarn or pnpm instead of npm set the setting `npm.packageManager` or `eslint.packageManager` to either `yarn` or `pnpm`' : null,
 					`Alternatively you can disable ESLint for the workspace folder ${workspaceFolder.name} by executing the 'Disable ESLint' command.`
 				].filter((str => (str !== null))).join('\n'));
 
@@ -1873,7 +1873,7 @@ function realActivate(context: ExtensionContext): void {
 				client.info([
 					`Failed to load the ESLint library for the document ${uri.fsPath}`,
 					`To use ESLint for single JavaScript file install eslint globally using '${globalInstall[packageManager]}'.`,
-					isPackageManagerNpm ? 'If you are using yarn or pnpm instead of npm set the setting `eslint.packageManager` or `npm.packageManager` to either `yarn` or `pnpm`' : null,
+					isPackageManagerNpm ? 'If you are using yarn or pnpm instead of npm set the setting `npm.packageManager` or `eslint.packageManager` to either `yarn` or `pnpm`' : null,
 					'You need to reopen VS Code after installing eslint.',
 				].filter((str => (str !== null))).join('\n'));
 
