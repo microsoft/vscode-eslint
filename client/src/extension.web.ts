0cd5665e06038662008cfcdc261edb9d963d7a58
ChaseKnowlden <haroldknowlden@gmail.com>
2024-07-09 11:21:10 -0400

    Initial draft of the Web Extension

---
+++ client/src/extension.web.ts
@@ -0,0 +1,48 @@
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ExtensionContext, Uri } from 'vscode';
+import { LanguageClientOptions } from 'vscode-languageclient';
+
+import { LanguageClient } from 'vscode-languageclient/browser';
+
+let client: LanguageClient | undefined;
+// this method is called when vs code is activated
+export async function activate(context: ExtensionContext) {
+
+	/*
+	 * all except the code to create the language client in not browser specific
+	 * and could be shared with a regular (Node) extension
+	 */
+	const documentSelector = [{ language: 'plaintext' }];
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		documentSelector,
+		synchronize: {},
+		initializationOptions: {}
+	};
+
+	client = createWorkerLanguageClient(context, clientOptions);
+
+	await  client.start();
+	console.log('lsp-web-extension-sample server is ready');
+}
+
+export async function deactivate(): Promise<void> {
+	if (client !== undefined) {
+		await client.stop();
+	}
+}
+
+function createWorkerLanguageClient(context: ExtensionContext, clientOptions: LanguageClientOptions) {
+	// Create a worker. The worker main file implements the language server.
+	const serverMain = Uri.joinPath(context.extensionUri, 'server/dist/browserServerMain.js');
+	const worker = new Worker(serverMain.toString(true));
+
+	// create the language server client to communicate with the server running in the worker
+	return new LanguageClient('lsp-web-extension-sample', 'LSP Web Extension Sample', clientOptions, worker);
+}
